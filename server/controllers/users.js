499c1fd78bac673fa54685f8c7cc18493efa9288
Aliaksandr Bruzgin <aliaksandr_bruzgin@epam.com>
2017-03-20 14:44:36 +0300

    extracted strategies to a separate file

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,66 +1,9 @@
 import { Strategy as LocalStrategy } from 'passport-local';
-import jwt from 'jsonwebtoken';
 import passport from 'passport';
 import User from '../models/users';
-import config from './../config/config.json';
-import sequelize from './../utils/sequelize';
+import passportConfig from './../config/strategies';
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-// used to deserialize the user
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
-
-passport.use('local-sign-up', new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  session: config.jwtCreds.session,
-  passReqToCallback: true,
-}, (req, email, password, done) => {
-  User.findOne({
-    where: {
-      email
-    }
-  })
-  .then((user) => {
-    if (user) {
-      return done(null, false, {
-        success: false,
-        name: 'Wrong email',
-        message: 'A user with such email already exists',
-      });
-    }
-    const confirmPassword = req.body.confirmPassword;
-    const enteredPassword = password;
-    
-    if (enteredPassword !== confirmPassword) {
-      return done(null, false, {
-        sucsess: false,
-        name: 'Wrong password confirmation',
-        message: 'Your passwords do not match. Please, write the correct passwords',
-      });
-    }
-    User.create({
-      email,
-      password,
-      username: req.body.username,
-      role: req.body.role,
-    })
-      .then(user => done(null, null, {
-        message: `A new user '${user.username}' with role '${user.role}' has been created`
-      }))
-      .catch(err => done(null, false, {message: err}));
-  })
-  .catch(err => done(null, false, {
-        success: false,
-        message: err,
-  }));
-}));
+passportConfig(passport);
 
 export function signUp(req, res) {
   passport.authenticate('local-sign-up', (err, newUser, info) => {
@@ -76,63 +19,6 @@ export function signUp(req, res) {
     res.json(info);
   })(req, res);
 }
-passport.use('local-sign-in', new LocalStrategy({
-  usernameField: 'email',
-  passwordField: 'password',
-  session: config.jwtCreds.session,
-  passReqToCallback: true,
-}, (req, email, password, done) => {
-  User.findOne({
-    where:{
-      email
-    }
-  })
-  // sequelize.query('SELECT * FROM users WHERE email = :email', {
-  //   type: Sequelize.QueryTypes.SELECT,
-  //   model: User,
-  //   replacements: {
-  //     email
-  //   }
-  // })
-  .then((user) => {
-    // const foundUser = user[0].dataValues;
-    if (!user) {
-      return done(null, false, {
-        success: false,
-        name: 'Non-existence',
-        message: 'A user with such email does not exist',
-      });
-    }
-    const payload = {
-      id: user.id,
-    };
-    const token = jwt.sign(payload, config.jwtCreds.secret);
-    const data = {
-      email: user.email,
-      username: user.username,
-      success: true,
-    };
-    // пока без солей
-
-    return done(null, token, data);
-    // user.comparePassword(password, (error, match) => {
-    //   if (match) {
-    //     return done(null, token, data);
-    //   } else {
-    //     return done(null, false, {
-    //       success: false,
-    //       message: 'Incorrect password.',
-    //     });
-    //   }
-    // });
-  })
-  .catch(err => done(null, false, {
-        success: false,
-        message: err,
-      }
-    )
-  );
-}));
 
 // sign-in middleware
 export function signIn(req, res) {
