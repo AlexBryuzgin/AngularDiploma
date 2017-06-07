62ff9b0f13b2540f5be530ed6ccbc76f2e331066
Александр Брюзгин < >
2017-06-08 00:20:20 +0300

    some styles

--- client/src/scripts/components/Header/Header.jsx
+++ client/src/scripts/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className='header'>
       <div className="header__container">
-        <Link className="header__logo" to='/'>My Project</Link>
+        <Link className="header__logo" to='/'>Bet 'n' Buy</Link>
         <UserBarContainer />
       </div>
     </header>
